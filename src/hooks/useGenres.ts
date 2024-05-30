9acb40bb03b74d06fa0369f2be2a9a60843df3ed
dsubiros <dsubiros@gmail.com>
2024-05-30 16:47:23 -0400

    Working on useGenres

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import genres from "../data/genres";
+import useData from "./useData";
 
 export interface Genre {
   id: number;
@@ -6,6 +7,21 @@ export interface Genre {
   image_background: string;
 }
 
-const useGenres = () => ({ data: genres, isLoading: false, error: null })
+// const useGenres = () => ({ data: genres, isLoading: false, error: null })
+const useGenres = () => useData<Genre>("/genres");
+
+// const useGames = (gameQuery: GameQuery) =>
+//   useData<Game>(
+//     "/games",
+//     {
+//       params: {
+//         genres: gameQuery.genre?.id,
+//         platforms: gameQuery.platform?.id,
+//         ordering: gameQuery.sortOrder,
+//         search: gameQuery.searchText
+//       },
+//     },
+//     [gameQuery]
+//   );
 
 export default useGenres;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    // key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "287fb05a407d45eb994ed52daed60eae",
   },
 });
