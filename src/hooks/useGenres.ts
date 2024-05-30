52075e0758e271821a9e4f10f5b2360d14224130
Dariel Subiros <dsubiros@gmail.com>
2024-05-30 19:46:07 -0400

    Load Genres using React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
+import { CACHE_KEY_GENRES } from "../constants";
 import genres from "../data/genres";
-import useData from "./useData";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
   id: number;
@@ -7,21 +10,24 @@ export interface Genre {
   image_background: string;
 }
 
-// const useGenres = () => ({ data: genres, isLoading: false, error: null })
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => {
+  const controller = new AbortController();
 
-// const useGames = (gameQuery: GameQuery) =>
-//   useData<Game>(
-//     "/games",
-//     {
-//       params: {
-//         genres: gameQuery.genre?.id,
-//         platforms: gameQuery.platform?.id,
-//         ordering: gameQuery.sortOrder,
-//         search: gameQuery.searchText
-//       },
-//     },
-//     [gameQuery]
-//   );
+  const query = useQuery<FetchResponse<Genre>, Error>({
+    queryKey: CACHE_KEY_GENRES,
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Genre>>("/genres", {
+          signal: controller.signal,
+          // ...requestConfig
+        })
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genres.length, results: genres },
+  });
+
+  // return useData<Genre>("/genres");
+  return query;
+};
 
 export default useGenres;
