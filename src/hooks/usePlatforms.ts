0e454a4dacfeb42243404ad04ac1591c88f2c830
Dariel Subiros <dsubiros@gmail.com>
2024-05-30 20:05:21 -0400

    Working on loading Platforms using React Query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,7 @@
-import platforms from "../data/platforms";
+import { useQuery } from "@tanstack/react-query";
+import { CACHE_KEY_PLATFORMS } from "../constants";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 interface Platform {
   id: number;
@@ -6,6 +9,12 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+// const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const usePlatforms = () =>
+  useQuery<FetchResponse<Platform>, Error>({
+    queryKey: CACHE_KEY_PLATFORMS,
+    queryFn: () => apiClient.get<FetchResponse<Platform>>("/platforms"),
+    staleTime: 24 * 60 * 60 * 1000,
+  });
 
 export default usePlatforms;
