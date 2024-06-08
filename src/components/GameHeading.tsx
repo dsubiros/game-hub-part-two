8786e8f5a8d707727d67557b115bdcd7b6179494
Dariel Subiros <dsubiros@gmail.com>
2024-06-08 18:21:30 -0400

    Fix Game Heading

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,13 +1,23 @@
 import { Heading } from '@chakra-ui/react'
 import { GameQuery } from '../App'
+import useGenres from '../hooks/useGenres'
+import usePlatforms from '../hooks/usePlatforms'
 
 interface Props {
   gameQuery: GameQuery
 }
 
-const GameHeading = ({ gameQuery }: Props) => {
+const GameHeading = ({ gameQuery: {platformId, genreId} }: Props) => {
   // const heading = `${gameQuery.platform?.name || ''} ${gameQuery.genre?.name || ''} Games`;
-  const heading = `${gameQuery.platformId || ''} ${gameQuery.genreId || ''} Games`;
+  // const heading = `${gameQuery.platformId || ''} ${gameQuery.genreId || ''} Games`;
+  
+  const {data: platforms} = usePlatforms();
+  const {data: genres} = useGenres();
+
+  const platform = platforms.results.find(p => p.id === platformId);
+  const genre = genres.results.find(p => p.id === genreId);
+
+  const heading = `${platform?.name || ''} ${genre?.name || ''} Games`;
 
   return (
     <Heading as='h1' marginY={5} fontSize='5xl'>{heading}</Heading>
